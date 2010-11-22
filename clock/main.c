ec26b2e4b1fae7b14c3af35a4564733334126826
Ali Raheem <ali.raheem@gmail.com>
2010-11-22 17:03:52 +0000

    Some major bugs. Alarm ultrasonic. And mins wont work with the second blinker on - don't ask me

--- clock/main.c
+++ clock/main.c
@@ -5,43 +5,63 @@ unsigned int at _CONFIG1 configWord1 = 0x20E4;
 unsigned int at _CONFIG2 configWord2 = 0x0700;
 //These are settings and the variables we use to store values.
 #define	DELAY 59016	//I let it run, the longer the better try a few hours.
-unsigned char secs = 0;
-unsigned char mins = 22;
-unsigned char hours = 3;
+struct time {
+	unsigned char secs;
+	unsigned char mins;
+	unsigned char hours;
+};
+struct time time;
+struct time alarm;
+
 //Prototypes
+void pause(unsigned int);
 void min(void);
 void hour(void);
-void pause(unsigned int);
 void init(void);
 
 void main(void){
 	init();
 	for(;;){
-		if(++secs == 60){
-			secs = 0;
+		if(++time.secs == 60){
+			time.secs = 0;
 			min();
 		}
+		PORTA ^= 0x80;
 		pause(DELAY); // Empirical value defined above.
 	}
 }
 void init(void){
-	TRISA = 0; //Minutes
+	//Time set
+	time.secs = 0;
+	time.mins = 10;
+	time.hours = 10;
+	//Alarm set
+	alarm.mins = 11;
+	alarm.hours = 10;
+	//0utput and 1nput
+	TRISA = 0; //Minutes and second blinker
+	TRISB = 0; //Alarm bell
 	TRISC = 0; //Hours
-	PORTA = mins;
-	PORTC = hours;
+	//Set output
+	PORTA = time.mins;
+	PORTB = 0;
+	PORTC = time.hours;
 }
 void pause(unsigned int delay){
 	unsigned int now = 0;
-	while(now++ < delay);
+	while(now++ < delay){
+		if(alarm.hours == time.hours && alarm.mins == time.mins)
+			PORTB ^= 0xFF;
+	}
 }
 void min(void){
-	if(++mins == 60){
-		mins = 0;
+	if(++time.mins == 60){
+		time.mins = 0;
 		hour();
 	}
-	PORTA = mins;
+	PORTA = time.mins;
 }
 void hour(void){
-	if(++hours == 12) hours = 0;
-	PORTC = hours;
+	if(++time.hours == 12) time.hours = 0;
+	PORTC = time.hours;
 }
