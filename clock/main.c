3f742f48c29df18ebbd6f36a5ae62556186b16ce
Ali Raheem <ali.raheem@gmail.com>
2010-11-22 19:00:57 +0000

    Fixed a stupid bug. time assigned in decalred order

--- clock/main.c
+++ clock/main.c
@@ -4,15 +4,13 @@
 unsigned int at _CONFIG1 configWord1 = 0x20E4;
 unsigned int at _CONFIG2 configWord2 = 0x0700;
 //These are settings and the variables we use to store values.
-#define	DELAY 59016	//I let it run, the longer the better try a few hours.
+#define	DELAY 59016	//Might need to tweak this.
 struct time {
-	unsigned char secs;
-	unsigned char mins;
-	unsigned char hours;
+	char hours;
+	char mins;
+	char secs;
 };
 struct time time;
-struct time alarm;
-
 //Prototypes
 void pause(unsigned int);
 void min(void);
@@ -26,33 +24,23 @@ void main(void){
 			time.secs = 0;
 			min();
 		}
-		PORTA ^= 0x80;
-		pause(DELAY); // Empirical value defined above.
+		pause(DELAY); //Empirical value defined above.
 	}
 }
 void init(void){
-	//Time set
-	time.secs = 0;
-	time.mins = 10;
-	time.hours = 10;
-	//Alarm set
-	alarm.mins = 11;
-	alarm.hours = 10;
 	//0utput and 1nput
-	TRISA = 0; //Minutes and second blinker
-	TRISB = 0; //Alarm bell
-	TRISC = 0; //Hours
-	//Set output
+	TRISA = 0b11000000; //Minutes
+	TRISC = 0b11110000; //Hours
+	//Time set
+	time.hours = 10;
+	time.mins = 30;
+	time.secs = 0;
 	PORTA = time.mins;
-	PORTB = 0;
 	PORTC = time.hours;
 }
 void pause(unsigned int delay){
 	unsigned int now = 0;
-	while(now++ < delay){
-		if(alarm.hours == time.hours && alarm.mins == time.mins)
-			PORTB ^= 0xFF;
-	}
+	while(now++ < delay);
 }
 void min(void){
 	if(++time.mins == 60){
